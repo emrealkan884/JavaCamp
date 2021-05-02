647df2be91340b93ef773eaf54e41ffd92975c7e
emrealkan884 <Hasan.alkan884@gmail.com>
2021-05-02 23:30:59 +0300

    First Commit

--- JavaCamp/src/Week4/EDevletValidator.java
+++
@@ -1,9 +0,0 @@
-package Week4;
-
-import Week4.Entities.Customer;
-
-public class EDevletValidator {
-	public void Validate(Customer customer) {
-		System.out.println("E-Devlet ile Validate edildi");
-	}
-}
